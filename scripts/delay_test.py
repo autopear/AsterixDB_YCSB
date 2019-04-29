e9aeb465bc5cb7a6896814def9b7f633a01995e6
Merlin <lonelystars@gmail.com>
2019-04-29 14:08:01 -0700

    Update

--- scripts/delay_test.py
+++ scripts/delay_test.py
@@ -45,6 +45,39 @@ def ycsb_job(is_load, k, workload, num_threads, ops):
     call(cmd, shell=True)
 
 
+def list_files(delay):
+    def print_size(s):
+        if s < 1024:
+            return  "{}b".format(s)
+        elif s < 1024 ** 2:
+            return "{:.1f}k".format(round(s / 1024))
+        elif s < 1024 ** 3:
+            return "{:.1f}m".format(round(s / (1024 ** 2)))
+        else:
+            return "{:.1f}g".format(round(s / (1024 ** 3)))
+
+    data_dir = os.path.join(asterixdb, "data", "red", "storage", "partition_0", "ycsb", "usertable",
+                            "0", "usertable")
+
+    while True:
+        if os.path.isdir(data_dir):
+            files = []
+            num_files = 0
+            total_size = 0
+            for f in sorted(glob.glob(os.path.join(data_dir, "*_b"))):
+                try:
+                    size = int(os.path.getsize(f))
+                    files.append("{}: {}".format(os.path.basename(f), print_size(size)))
+                    num_files += 1
+                    total_size += size
+                except:
+                    pass
+            print("Total {}: {}".format(num_files, print_size(total_size)))
+            if num_files > 0:
+                print(", ".join(files))
+        time.sleep(delay)
+
+
 def start_server():
     call("\"" + asterixdb + "/bin/start-sample-cluster.sh\"", shell=True)
 
@@ -194,7 +227,10 @@ def zip_log(zip_path, file_path):
     with zipfile.ZipFile(zip_path, "w") as z:
         z.write(file_path, os.path.basename(file_path), zipfile.ZIP_DEFLATED)
     z.close()
-    os.remove(file_path)
+    try:
+        os.remove(file_path)
+    except:
+        pass
 
 
 def grep_logs(k):
@@ -203,16 +239,16 @@ def grep_logs(k):
     mergen = "merges_" + str(k) + ".csv"
     readn = "reads_" + str(k) + ".csv"
 
-    propsf = open(os.path.join(logs_dir, propsn), "a")
-    flushf = open(os.path.join(logs_dir, flushn), "a")
-    mergef = open(os.path.join(logs_dir, mergen), "a")
-    readf = open(os.path.join(logs_dir, readn), "a")
+    propsf = open(os.path.join(logs_dir, propsn), "w")
+    flushf = open(os.path.join(logs_dir, flushn), "w")
+    mergef = open(os.path.join(logs_dir, mergen), "w")
+    readf = open(os.path.join(logs_dir, readn), "w")
 
     for logp in glob.glob(os.path.join(asterixdb, "logs", "*.gz")):
         with gzip.open(logp, "rt") as inf:
             for line in inf:
                 if "[PROPERTIES]" in line:
-                    line = line[line.index("[FLUSH]")+8:].replace("\r", "")
+                    line = line[line.index("[PROPERTIES]")+13:].replace("\r", "")
                     propsf.write(line.replace(":", ": ").replace(",", "\n") + "\n")
                 if "[FLUSH]" in line:
                     flushf.write(line[line.index("[FLUSH]")+8:].replace("\r", ""))
@@ -230,7 +266,7 @@ def grep_logs(k):
         with io.open(logp, "r", encoding="utf-8") as inf:
             for line in inf:
                 if "[PROPERTIES]" in line:
-                    line = line[line.index("[FLUSH]")+8:].replace("\r", "")
+                    line = line[line.index("[PROPERTIES]")+13:].replace("\r", "")
                     propsf.write(line.replace(":", ": ").replace(",", "\n") + "\n")
                 if "[FLUSH]" in line:
                     flushf.write(line[line.index("[FLUSH]")+8:].replace("\r", ""))
@@ -284,24 +320,30 @@ def run_exp(k):
         return False
     print("Feed started")
 
+    propsn = "props_" + str(k) + ".txt"
     flushn = "flushes_" + str(k) + ".csv"
     mergen = "merges_" + str(k) + ".csv"
     readn = "reads_" + str(k) + ".csv"
 
-    for n in (flushn, mergen, readn):
+    for n in (propsn, flushn, mergen, readn):
         try:
             os.remove(os.path.join(logs_dir, n))
         except:
             pass
 
     p_write = mp.Process(target=ycsb_job, args=(True, k, "write", 4, 0))
-    p_read = mp.Process(target=ycsb_job, args=(False, k, "read", 4, 0))
+    p_read = mp.Process(target=ycsb_job, args=(False, k, "read", 1, 0))
+    p_list = mp.Process(target=list_files, args=(10,))
 
+    p_list.start()
     p_write.start()
+    time.sleep(10)
     p_read.start()
 
     p_write.join()
     p_read.join()
+
+    p_list.terminate()
 
     wait_merge(k)
     grep_logs(k)
